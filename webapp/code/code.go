17e19e7980a7c1efe63baa3fa9c5da111846bfb3
Gregg Townsend <gmt@cs.arizona.edu>
2014-04-29 13:29:48 -0700

    Use templates for most output.  Sanitize all user strings.

--- webapp/code/code.go
+++ webapp/code/code.go
@@ -4,8 +4,10 @@ package code
 
 import (
 	"appengine"
+	"bytes"
 	"fmt"
 	"html"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"math/rand"
@@ -41,7 +43,7 @@ func response(w http.ResponseWriter, r *http.Request) {
 
 	putheader(w, r, "Response")
 	showexpr(w, content)
-	fmt.Fprintf(w, "<h2>Try another?</h2>")
+	fmt.Fprint(w, "<h2>Try another?</h2>")
 	putform(w, "Enter a regular expression:")
 	putfooter(w, r)
 }
@@ -53,11 +55,10 @@ func showexpr(w http.ResponseWriter, s string) {
 		fmt.Fprintf(w, "<P>ERROR: %s\n", hx(err))
 		return
 	}
-	fmt.Fprintf(w, "<P>Expression: %s\n", s)
-	fmt.Fprintf(w, "<P>Parse Tree: %s\n", hx(tree))
+	fmt.Fprintf(w, "<P>Expression: %s\n", hx(s))
+	fmt.Fprintf(w, "<P>Parse Tree: %s\n", hx(tree)) // print before augment!
 
 	//#%#% currently no protection against DOS attempt from huge expr
-	//#%#% likewise input strings are not fully sanitized
 	augt := rx.Augment(tree, 0)
 	dfa := rx.BuildDFA(augt)
 	dmin := dfa.Minimize()
@@ -70,19 +71,22 @@ func showexpr(w http.ResponseWriter, s string) {
 	showexamples(w, tree, 3)
 	showexamples(w, tree, 5)
 	showexamples(w, tree, 8)
-	fmt.Fprintf(w, "<h2>NFA</h2>\n<PRE>\n")
-	dfa.ShowNFA(w, "")
-	fmt.Fprintf(w, "</PRE>\n")
-	fmt.Fprintf(w, "<h2>DFA</h2>\n<PRE>\n")
-	dmin.DumpStates(w, "")
-	fmt.Fprintf(w, "</PRE>\n")
+
+	nfaBuffer := &bytes.Buffer{}
+	dfa.ShowNFA(nfaBuffer, "")
+	fmt.Fprintf(w, "<h2>NFA</h2><PRE>\n%s</PRE>\n",
+		hx(string(nfaBuffer.Bytes())))
+
+	dfaBuffer := &bytes.Buffer{}
+	dmin.DumpStates(dfaBuffer, "")
+	fmt.Fprintf(w, "<h2>DFA</h2><PRE>\n%s</PRE>\n",
+		hx(string(dfaBuffer.Bytes())))
 }
 
 const linemax = 79 // max output line length for examples
 
 //  showexamples writes a line of specimen strings matching the expression
 func showexamples(w http.ResponseWriter, tree rx.Node, maxrepl int) {
-	fmt.Fprintf(w, "<BR>")
 	nprinted := 0
 	ncolm := 0
 	for {
@@ -95,85 +99,96 @@ func showexamples(w http.ResponseWriter, tree rx.Node, maxrepl int) {
 		fmt.Fprintf(w, " %s &nbsp; ", hx(t))
 		nprinted++
 	}
-	fmt.Fprintf(w, "\n")
+	fmt.Fprint(w, "<BR>\n")
 }
 
 //  info writes data that is useful in debugging the application
 func info(w http.ResponseWriter, r *http.Request) {
+
 	// must do all reading before any writing
-	var body []byte
-	var berr error
-	if r.Body != nil {
-		body, berr = ioutil.ReadAll(r.Body)
+	var data struct {
+		Req   *http.Request
+		Body  []byte
+		BE    error
+		Dctr  string
+		VID   string
+		Vtime string
 	}
+	data.Req = r
+	data.Body, data.BE = ioutil.ReadAll(r.Body)
+	data.Dctr = appengine.Datacenter()
+	data.VID = appengine.VersionID(appengine.NewContext(r))
 
-	putheader(w, r, "Info")
-	fmt.Fprintf(w, "<P>Host: %#v\n", r.Host)
-	fmt.Fprint(w, "<P>")
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "%s : %s<BR>\n", k, v)
-	}
-
-	if berr != nil {
-		fmt.Fprintf(w, "<P>BODY ERROR: %s\n", berr)
-	}
-	fmt.Fprintf(w, "<P>Body:<BR>%s\n", hx(body))
-
-	fmt.Fprintf(w, "<P>Datacenter: %s\n", appengine.Datacenter())
-
-	vid := appengine.VersionID(appengine.NewContext(r))
 	var ver int
 	var bigtime int64
-	fmt.Sscanf(vid, "%d.%d", &ver, &bigtime)
+	fmt.Sscanf(data.VID, "%d.%d", &ver, &bigtime)
 	// the following is correct for App Engine but not for SDK
 	t := time.Unix(bigtime>>28, 0)
-	fmt.Fprintf(w, "<P>App Version ID: %s (%s)\n", vid,
-		t.Format("01/02 15:04"))
+	data.Vtime = t.Format("01/02 15:04")
 
+	putheader(w, r, "Info")
+	tInfo.Execute(w, data)
 	putfooter(w, r)
 }
 
+var tInfo = template.Must(template.New("header").Parse(
+	`<P>Host: {{.Req.Host}}
+<BR>Datacenter: {{.Dctr}}
+<BR>App Version ID: {{.VID}} ({{.Vtime}})
+<H2>Request Header</H2>
+<P>{{range $k, $v := .Req.Header}}{{$k}} : {{$v}}<BR>{{end}}
+<H2>Request Body</H2>
+<P>{{printf "%s" .Body}}
+{{if .BE}}<P><B>BODY ERROR:</B> {{.BE}}{{end}}
+`))
+
 //  putheader outputs our standard HTML page header
 func putheader(w io.Writer, r *http.Request, title string) {
-	prefix := "RX"
-	favicon := "icon.png"
+	data := struct{ Prefix, Title, Favicon string }{
+		"RX", title, "icon.png"}
 	if r.Host == "localhost:8080" {
-		favicon = "ilocal.png"
+		data.Favicon = "itest.png"
 	}
-	fmt.Fprintf(w, `
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+	tHeader.Execute(w, data)
+}
+
+var tHeader = template.Must(template.New("header").Parse(
+	`<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html><head>
-<title>%s: %s</title>
+<title>{{.Prefix}}: {{.Title}}</title>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 <link rel="stylesheet" type="text/css" href="/static/style.css">
-<link rel="icon" type="image/png" href="/static/%s">
+<link rel="icon" type="image/png" href="/static/{{.Favicon}}">
 </head><body>
-<h1>%s: %s</h1>
-`, prefix, title, favicon, prefix, title)
-}
+<h1>{{.Prefix}}: {{.Title}}</h1>
+`))
 
 //  putform outputs a form for submitting an expression
 func putform(w io.Writer, label string) {
-	fmt.Fprintf(w, `
-<P>%s
+	tForm.Execute(w, label)
+}
+
+var tForm = template.Must(template.New("form").Parse(
+	`<P>{{.}}
 <P><form action="/response" method="post">
 <div><input type="text" name="content" size=60 maxlength=1000></div>
 <div><input type="submit" value="Submit"></div>
 </form>
-`, label)
-}
+`))
 
 //  putfooter outputs our standard HTML page footer
 func putfooter(w io.Writer, r *http.Request) {
-	fmt.Fprintf(w, `
-<P><BR><BR><HR>
+	tFooter.Execute(w, 1000+rand.Intn(9000))
+}
+
+var tFooter = template.Must(template.New("footer").Parse(
+	`<P><BR><BR><HR>
 <P style="text-align:left;">
 <A title="Home" href="/">Home</a>
 <span style="float:right;">
-RX %d
+RX {{.}}
 <A title="Info" href="/info">I</a>
 <A title="Val" href="http://validator.w3.org/check?uri=referer&amp;ss=1">V</a>
 </span> 
 </body></html>
-`, 1000+rand.Intn(9000))
-}
+`))
