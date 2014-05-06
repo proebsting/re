6d343272158b83a6f4f862994da00e320990650b
Gregg Townsend <gmt@cs.arizona.edu>
2014-05-06 13:17:14 -0700

    Compare multiple expressions in webapp.  (Initial version.)

--- webapp/code/code.go
+++ webapp/code/code.go
@@ -28,13 +28,30 @@ var examples = []struct{ Expr, Caption string }{
 	{`\w+@\w+(\.\w+)+`, "Naive e-mail address"},
 }
 
+var multixamples = []struct {
+	Caption string
+	Exprs   []string
+}{
+	{"Decimal numbers", []string{
+		`[1-9]\d*`, `0|-?[1-9]\d*`, `\d*\.\d+|\d+\.\d*`,
+		`-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d{1,3})?`}},
+	{"Binary integers", []string{
+		`[01]+`, `0|1[01]*`, `(0|1(01*0)*1)*`, `1?(01)*0?`}},
+	{"US Social Security Numbers", []string{
+		`\d\d\d-\d\d-\d\d\d\d`, `\d\d\d-(\d[1-9]|[1-9]\d)-\d\d\d\d`,
+		`([0-6]\d{2}|7([0-6]\d|7[012]))-\d\d-\d\d\d\d`}},
+}
+
 //  init registers URLs for dispatching and sets a random seed
 func init() {
-	http.HandleFunc("/", home) // anything else
+	http.HandleFunc("/", examine)        // anything unmatched
+	http.HandleFunc("/examine", examine) // anything unmatched
+	http.HandleFunc("/compare", compare)
+	http.HandleFunc("/details", details)
+	http.HandleFunc("/combos", combos)
+	http.HandleFunc("/syntax", syntax)
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/info", info)
-	http.HandleFunc("/syntax", syntax)
-	http.HandleFunc("/response", response)
 	rand.Seed(int64(time.Now().Nanosecond()))
 }
 
@@ -43,79 +60,97 @@ func hx(s interface{}) string {
 	return html.EscapeString(fmt.Sprint(s))
 }
 
-//  home presents the home page
-func home(w http.ResponseWriter, r *http.Request) {
-	putheader(w, r, "Query")
-	putform(w, "Enter a regular expression:")
+//  examine presents a query page for examining a single expression
+func examine(w http.ResponseWriter, r *http.Request) {
+	putheader(w, r, "Inspection Query")
+	putform(w, 1, "/details", "Enter a regular expression:")
 	tExamples.Execute(w, examples)
 	putfooter(w, r)
 }
 
-var tExamples = template.Must(template.New("header").Parse(
+var tExamples = template.Must(template.New("examples").Parse(
 	`<P>Or choose one of these examples:{{range .}}
-<form action="/response" method="post"><div>
-<input type="hidden" name="content" value="{{.Expr}}">
+<form action="/details" method="post"><div>
+<input type="hidden" name="a0" value="{{.Expr}}">
 <button class=link>{{.Caption}}</button></div></form>{{end}}
 `))
 
-//  response presents a response to a form submission
-func response(w http.ResponseWriter, r *http.Request) {
-	// must do all reading before any writing
-	content := r.FormValue("content")
-
-	putheader(w, r, "Response")
-	showexpr(w, content)
-	fmt.Fprint(w, "<h2>Try another?</h2>")
-	putform(w, "Enter a regular expression:")
+//  compare presents a page asking for multiple expressions
+func compare(w http.ResponseWriter, r *http.Request) {
+	putheader(w, r, "Comparison Query")
+	putform(w, 5, "/combos", "Enter regular expressions:")
+	tMultiEx.Execute(w, multixamples)
 	putfooter(w, r)
 }
 
-//  showexpr generates information describing a regular expression
-func showexpr(w http.ResponseWriter, s string) {
-	tree, err := rx.Parse(s)
-	if err != nil {
-		if pe, ok := err.(*rx.ParseError); ok {
-			fmt.Fprintf(w,
-				"<P><B>Error:</B> %s\n<BR>In expression: %s\n",
-				pe.Message, hx(pe.BadExpr))
-		} else {
-			fmt.Fprintf(w, "<P><B>Error:</B> %s\n", hx(err))
-		}
-		return
+var tMultiEx = template.Must(template.New("multixamples").Parse(
+	`<P>Or choose one of these predefined sets:{{range .}}
+<form action="/combos" method="post"><div>{{range $i, $s := .Exprs}}
+<input type="hidden" name="a{{$i}}" value="{{$s}}">
+{{end}}
+<button class=link>{{.Caption}}</button></div></form>{{end}}
+`))
+
+//  details responds to an inspection request for a single expression
+func details(w http.ResponseWriter, r *http.Request) {
+	expr := r.FormValue("a0") // must read before any writing
+	putheader(w, r, "Inspect Expression")
+	tree, err := rx.Parse(expr)
+	if err == nil {
+		fmt.Fprintf(w, "<P>Regular Expression: %s\n", hx(expr))
+		// must print (or at least stringize) tree before augmenting
+		fmt.Fprintf(w, "<P>Initial Parse Tree: %s\n", hx(tree))
+
+		//#%#% currently no protection against DOS by huge expr
+		augt := rx.Augment(tree, 0)
+		dfa := rx.BuildDFA(augt)
+		dmin := dfa.Minimize()
+
+		fmt.Fprintf(w, "<P>Augmented Tree: %s\n", hx(augt))
+		fmt.Fprintf(w, "<h2>Examples</h2>\n<P>")
+		genexamples(w, tree, 0)
+		genexamples(w, tree, 1)
+		genexamples(w, tree, 2)
+		genexamples(w, tree, 3)
+		genexamples(w, tree, 5)
+		genexamples(w, tree, 8)
+
+		nfaBuffer := &bytes.Buffer{}
+		dfa.ShowNFA(nfaBuffer, "")
+		fmt.Fprintf(w, "<h2>NFA</h2><PRE>\n%s</PRE>\n",
+			hx(string(nfaBuffer.Bytes())))
+
+		dfaBuffer := &bytes.Buffer{}
+		dmin.DumpStates(dfaBuffer, "")
+		fmt.Fprintf(w, "<h2>DFA</h2><PRE>\n%s</PRE>\n",
+			hx(string(dfaBuffer.Bytes())))
+	} else {
+		showerror(w, err)
 	}
-	fmt.Fprintf(w, "<P>Regular Expression: %s\n", hx(s))
-	// must print (or at least stringize) tree before augmenting
-	fmt.Fprintf(w, "<P>Initial Parse Tree: %s\n", hx(tree))
 
-	//#%#% currently no protection against DOS attempt from huge expr
-	augt := rx.Augment(tree, 0)
-	dfa := rx.BuildDFA(augt)
-	dmin := dfa.Minimize()
+	fmt.Fprint(w, "<h2>Try another?</h2>")
+	putform(w, 1, "/details", "Enter a regular expression:")
+	putfooter(w, r)
+}
 
-	fmt.Fprintf(w, "<P>Augmented Tree: %s\n", hx(augt))
-	fmt.Fprintf(w, "<h2>Examples</h2>\n<P>")
-	showexamples(w, tree, 0)
-	showexamples(w, tree, 1)
-	showexamples(w, tree, 2)
-	showexamples(w, tree, 3)
-	showexamples(w, tree, 5)
-	showexamples(w, tree, 8)
-
-	nfaBuffer := &bytes.Buffer{}
-	dfa.ShowNFA(nfaBuffer, "")
-	fmt.Fprintf(w, "<h2>NFA</h2><PRE>\n%s</PRE>\n",
-		hx(string(nfaBuffer.Bytes())))
-
-	dfaBuffer := &bytes.Buffer{}
-	dmin.DumpStates(dfaBuffer, "")
-	fmt.Fprintf(w, "<h2>DFA</h2><PRE>\n%s</PRE>\n",
-		hx(string(dfaBuffer.Bytes())))
+//  showerror displays an error and returns true if its argument is not nil
+func showerror(w http.ResponseWriter, err error) bool {
+	if err == nil {
+		return false
+	}
+	if pe, ok := err.(*rx.ParseError); ok {
+		fmt.Fprintf(w, "<P><B>Error:</B> %s\n<BR>In expression: %s\n",
+			pe.Message, hx(pe.BadExpr))
+	} else {
+		fmt.Fprintf(w, "<P><B>Error:</B> %s\n", hx(err))
+	}
+	return true
 }
 
 const linemax = 79 // max output line length for examples
 
-//  showexamples writes a line of specimen strings matching the expression
-func showexamples(w http.ResponseWriter, tree rx.Node, maxrepl int) {
+//  genexamples writes a line of specimen strings matching the expression
+func genexamples(w http.ResponseWriter, tree rx.Node, maxrepl int) {
 	nprinted := 0
 	ncolm := 0
 	for {
@@ -131,6 +166,68 @@ func showexamples(w http.ResponseWriter, tree rx.Node, maxrepl int) {
 	fmt.Fprint(w, "<BR>\n")
 }
 
+//  combos responds to a comparison request for multiple expressions
+func combos(w http.ResponseWriter, r *http.Request) {
+
+	// must read all input before writing anything
+	labels := []string{"a0", "a1", "a2", "a3", "a4"}
+	elist := make([]string, 0, 5)
+	for _, l := range labels {
+		arg := r.FormValue(l)
+		if arg != "" {
+			elist = append(elist, arg)
+		}
+	}
+	n := len(elist)
+
+	tlist := make([]rx.Node, 0, n)
+	putheader(w, r, "Compare Expressions")
+	fmt.Fprintf(w, "<P>%d expressions:\n", n)
+	for i, s := range elist {
+		fmt.Fprintf(w, "<P><B>e%d:</B> &nbsp; %s\n", i, hx(s))
+		tree, err := rx.Parse(s)
+		if !showerror(w, err) {
+			tlist = append(tlist, rx.Augment(tree, uint(i)))
+		}
+	}
+
+	if n > 0 && len(tlist) == n { // if no errors
+		dfa := rx.MultiDFA(tlist)  // build combined DFA
+		xlist := make([]string, 0) // example list
+		synthx := dfa.Synthesize() // synthesize from DFA
+		for _, x := range synthx { // put results on list
+			xlist = append(xlist, x.Example)
+		}
+		showgrid(w, dfa, n, xlist) // show examples
+	}
+	fmt.Fprint(w, "<h2>Try again?</h2>")
+	putform(w, 5, "/combos", "Enter regular expressions:")
+	putfooter(w, r)
+}
+
+//  showgrid prints a table showing which expr matched which example
+func showgrid(w http.ResponseWriter, dfa *rx.DFA, nexpr int, xlist []string) {
+	fmt.Fprintf(w, "<H2>Results</H2>\n")
+	fmt.Fprintf(w, "<TABLE>\n<TR>")
+	for i := 0; i < nexpr; i++ {
+		fmt.Fprintf(w, "<TH>e%d</TH>", i)
+	}
+	fmt.Fprintf(w, "<TH>example</TH></TR>\n")
+	for _, s := range xlist {
+		fmt.Fprintf(w, "<TR>")
+		aset := dfa.Accepts(s)
+		for i := 0; i < nexpr; i++ {
+			if aset.Test(uint(i)) {
+				fmt.Fprintf(w, "<TD>\u2713</TD>") // ckmark
+			} else {
+				fmt.Fprintf(w, "<TD>\u2013</TD>") // ndash
+			}
+		}
+		fmt.Fprintf(w, "<TD>%s</TD></TR>\n", hx(s))
+	}
+	fmt.Fprintf(w, "</TABLE>\n")
+}
+
 //  about generates a page describing and crediting the website
 func about(w http.ResponseWriter, r *http.Request) {
 	putheader(w, r, "About")
@@ -138,7 +235,7 @@ func about(w http.ResponseWriter, r *http.Request) {
 	putfooter(w, r)
 }
 
-var tAbout = template.Must(template.New("header").Parse(`
+var tAbout = template.Must(template.New("about").Parse(`
 <P> This website is a work in progress by:
 <P> <A HREF="http://www.cs.arizona.edu/~proebsting">Todd Proebsting</A>
 <BR> <A HREF="http://www.cs.arizona.edu/~gmt">Gregg Townsend</A>
@@ -155,7 +252,7 @@ func syntax(w http.ResponseWriter, r *http.Request) {
 	putfooter(w, r)
 }
 
-var tSyntax = template.Must(template.New("header").Parse(`
+var tSyntax = template.Must(template.New("syntax").Parse(`
 <P>We implement traditional regular expressions with a few simple extensions.
 <P>The following forms are handled:<PRE>
       abc  a|b|c  a(b|c)d
@@ -210,7 +307,7 @@ func info(w http.ResponseWriter, r *http.Request) {
 	putfooter(w, r)
 }
 
-var tInfo = template.Must(template.New("header").Parse(
+var tInfo = template.Must(template.New("info").Parse(
 	`<P>Host: {{.Req.Host}}
 <BR>Datacenter: {{.Dctr}} ({{.GoArch}} {{.GoOs}})
 <BR>Go Version: {{.GoVer}}
@@ -243,18 +340,18 @@ var tHeader = template.Must(template.New("header").Parse(
 <h1>{{.Prefix}}: {{.Title}}</h1>
 `))
 
-//  putform outputs a form for submitting an expression
-func putform(w io.Writer, label string) {
-	tForm.Execute(w, label)
-}
-
-var tForm = template.Must(template.New("form").Parse(
-	`<P>{{.}}
-<form action="/response" method="post">
-<div><input type="text" name="content" size=60 maxlength=1000></div>
+//  putform outputs a form for submitting n expressions
+func putform(w io.Writer, n int, target string, label string) {
+	fmt.Fprintf(w, "<P>%s\n<form action=\"%s\" method=\"post\">",
+		label, target)
+	for i := 0; i < n; i++ {
+		fmt.Fprintf(w, `
+<div><input type="text" name="a%d" size=60 maxlength=1000></div>`, i)
+	}
+	fmt.Fprintln(w, `
 <div><input type="submit" value="Submit"></div>
-</form>
-`))
+</form>`)
+}
 
 //  putfooter outputs our standard HTML page footer
 func putfooter(w io.Writer, r *http.Request) {
@@ -265,6 +362,8 @@ var tFooter = template.Must(template.New("footer").Parse(
 	`<P><BR><BR><HR>
 <P style="text-align:left;">
 <A title="Home" href="/">Home</a>
+| <A title="Examine" href="/examine">Examine</a>
+| <A title="Compare" href="/compare">Compare</a>
 | <A title="Syntax" href="/syntax">Syntax</a>
 | <A title="About" href="/about">About</a>
 <span style="float:right;">
